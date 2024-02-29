2a4611d114e177f1707ca91ddde424fa529b5116
Isaac <isaac.farias@sesatech.com.br>
2024-02-29 16:59:08 -0300

    feat: create product route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::get('/products', [ProductController::class, 'index'])
+->name('products');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
