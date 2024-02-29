1cad981afc24d9b45f921ed9b7448612aaf4906e
Isaac <isaac.farias@sesatech.com.br>
2024-02-29 16:57:49 -0300

    feat: create category factory and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $category_id = Category::inRandomOrder()->first()->id;
+
+        $product = [
+            'name' => $this->faker->name,
+            'price' => $this->faker->randomFloat(2, 1, 1000),
+            'description' => $this->faker->text,
+            'category_id' => $category_id,
+            'status' => $this->faker->boolean,
+            'amount' => $this->faker->numberBetween(1, 100),
+        ];
+
+        return $product;
+    }
+}
