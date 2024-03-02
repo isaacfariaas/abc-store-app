841dfb3cf43ba101567b77faed0a727502b9557d
Isaac <isaac.farias@sesatech.com.br>
2024-03-02 14:41:30 -0300

    fix: Register of users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use DateInterval;
+use DateTimeImmutable;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -18,24 +20,29 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(Request $request)
     {
-        $request->validate([
+        if ($request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'string'],
+        ])) {
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-        event(new Registered($user));
+            event(new Registered($user));
 
-        Auth::login($user);
-
-        return response()->noContent();
+            Auth::login($user);
+            $expire_at = new DateTimeImmutable();
+            $expire_at = $expire_at->add(new DateInterval('PT1440M'));
+            $token = $user->createToken('JWT', ['*'], $expire_at);
+            return response()->json(["register" => true, "token" => $token->plainTextToken, "error" => ""], 200);
+        } else {
+            return response()->json(["register" => false, "token" => "", "error" => "User not enabled"], 401);
+        }
     }
 }
